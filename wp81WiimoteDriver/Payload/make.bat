b81df09e480277476d22698971ff163fc0c626bc
fredericGette <frederic.gette@gmail.com>
2025-01-25 20:28:06 +0100

    Cleaning

--- wp81WiimoteDriver/Payload/make.bat
+++ wp81WiimoteDriver/Payload/make.bat
@@ -1,6 +1,9 @@
 DEL *.obj
 DEL *.sys
 
+SETLOCAL
+SET PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_arm;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin;%PATH%
+
 CL.exe /c /I"C:\Program Files (x86)\Windows Phone Kits\8.1\Include\km" /I"C:\Program Files (x86)\Windows Kits\8.1\Include\Shared" /I"C:\Program Files (x86)\Windows Kits\8.1\Include\km" /I"C:\Program Files (x86)\Windows Kits\8.1\Include\wdf\kmdf\1.11" /I"C:\Program Files (x86)\Windows Kits\8.1\Include\km\crt" /Zi /W4 /WX /Od /D _ARM_ /D ARM /D _USE_DECLSPECS_FOR_SAL=1 /D STD_CALL /D DEPRECATE_DDK_FUNCTIONS=1 /D MSC_NOOPT /D _WIN32_WINNT=0x0602 /D WINVER=0x0602 /D WINNT=1 /D NTDDI_VERSION=0x06020000 /D DBG=1 /D _ARM_WINAPI_PARTITION_DESKTOP_SDK_AVAILABLE=1 /D KMDF_VERSION_MAJOR=1 /D KMDF_VERSION_MINOR=11 /Zp8 /Gy /Zc:wchar_t- /Zc:forScope- /GR- /wd4242 /wd4214 /wd4201 /wd4244 /wd4064 /wd4627 /wd4627 /wd4366 /wd4748 /wd4603 /wd4627 /wd4986 /wd4987 /wd4996 /wd4189 /wd4127 /FI"C:\Program Files (x86)\Windows Kits\8.1\Include\Shared\warning.h" /kernel /GF -cbstring /d1import_no_registry /d2AllowCompatibleILVersions /d2Zi+ driver.c device.c
 
 IF %ERRORLEVEL% NEQ 0 (
@@ -15,7 +18,7 @@ IF %ERRORLEVEL% NEQ 0 (
 
 REN driver.sys wp81wiimote.sys
 
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe" sign /ph /fd "sha256" /sha1 "38DD26500D3D48F0E8C6F73F58C5F08BE77F4B7D" wp81wiimote.sys
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe" sign /ph /fd "sha256" /f MySPC.pfx wp81wiimote.sys
 
 IF %ERRORLEVEL% NEQ 0 (
 	GOTO :EOF
@@ -36,7 +39,7 @@ IF %ERRORLEVEL% NEQ 0 (
 
 REN filter.sys wp81pairingfilter.sys
 
-"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe" sign /ph /fd "sha256" /sha1 "38DD26500D3D48F0E8C6F73F58C5F08BE77F4B7D" wp81pairingfilter.sys
+"C:\Program Files (x86)\Windows Kits\8.1\bin\x86\signtool.exe" sign /ph /fd "sha256" /f MySPC.pfx wp81pairingfilter.sys
 
 IF %ERRORLEVEL% NEQ 0 (
 	GOTO :EOF
